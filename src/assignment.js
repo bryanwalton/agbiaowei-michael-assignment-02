9c843463b47c1d997f4096925947996149e5577f
bryanwalton <bryanwalton1997@gmail.com>
2022-07-09 01:21:32 +0100

    Updated

--- src/assignment.js
+++ src/assignment.js
@@ -15,7 +15,7 @@ const assignment = {};
 function sumOfNumbers(arrayOfNumbers) {
     let total = 0;
     // for (let i = 0; i < arrayOfNumbers.length; i++) {
-    //     total += arrayOfNumbers[i];
+    //     total += arrayOfNumbers[i]};
 
     arrayOfNumbers.forEach((value) => {
         total += value;
